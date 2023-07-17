2de13f6757f96db916c615a0a78112a8e1e02dbc
Vid Tadel <tadelv@gmail.com>
2023-07-17 10:38:54 +0200

    Initial Commit

---
+++ Composition/CompositionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CompositionApp.swift
+//  Composition
+//
+//  Created by Vid Tadel on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CompositionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
